dfba89baa9436bdfbe3f61a1ea9a51d3a642ecc7
masaki sato <mimic52006@gmail.com>
2025-12-30 10:42:35 +0900

    Add progress updates in download-zip to make progress bar work

--- src/app/api/download-zip/route.ts
+++ src/app/api/download-zip/route.ts
@@ -1,5 +1,6 @@
 import JSZip from 'jszip'
 import { NextRequest } from 'next/server'
+import { progressMap } from '../download-progress/route'
 
 export const runtime = 'nodejs'
 
@@ -21,13 +22,20 @@ export async function POST(req: NextRequest) {
 
   console.log('Downloading images:', images.map((img: DownloadImage) => img.url))
 
+  progressMap.set(id, { progress: 0, total: images.length, complete: false })
+
   const zip = new JSZip()
 
+  let current = 0
   for (const img of images) {
     const res = await fetch(img.url)
     const buffer = await res.arrayBuffer()
     zip.file(`${img.id}.${format === 'original' ? 'jpg' : format}`, buffer)
+    current++
+    progressMap.set(id, { progress: current, total: images.length, complete: false })
   }
+
+  progressMap.set(id, { progress: images.length, total: images.length, complete: true })
 
   const zipBuffer = await zip.generateAsync({ type: 'arraybuffer' })
 
